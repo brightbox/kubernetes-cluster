dab972bc034ee7a1f517ce23894c597cf4faea43
Neil Wilson <neil@aldur.co.uk>
2018-08-31 16:58:35 +0100

    Bump cloud-controller to v0.0.9

--- variables.tf
+++ variables.tf
@@ -102,7 +102,7 @@ variable "containerd_release" {
 
 variable "brightbox_cloud_controller_release" {
   description = "Version of Brightbox cloud controller to use"
-  default     = "0.0.8"
+  default     = "0.0.9"
 }
 
 variable "runc_release" {
