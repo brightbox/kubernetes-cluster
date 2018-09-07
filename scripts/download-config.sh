4340283b4bfa40a01cb2493f7f9697d07e57119b
Neil Wilson <neil@aldur.co.uk>
2018-09-07 07:52:57 +0100

    Ensure download script is idempotent

--- scripts/download-config.sh
+++ scripts/download-config.sh
@@ -2,6 +2,6 @@
 # Download the config from the current cluster to the kubectl config
 # area on the current workstation
 set -e
-mkdir $HOME/.kube
+mkdir -p $HOME/.kube
 scp ubuntu@$(terraform output master):.kube/config ~/.kube/config
 sed -i "s/https:.*$/https:\/\/$(terraform output master):6443/" ~/.kube/config
