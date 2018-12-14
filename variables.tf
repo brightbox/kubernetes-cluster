431dfa0504555aee8459c37cb8a768185167e9dc
Neil Wilson <neil@aldur.co.uk>
2018-12-14 15:00:11 +0000

    Bump default to 1.13.1

--- variables.tf
+++ variables.tf
@@ -87,7 +87,7 @@ variable "cluster_name" {
 
 variable "kubernetes_release" {
   description = "Version of Kubernetes to install"
-  default     = "1.13.0"
+  default     = "1.13.1"
 }
 
 variable "calico_release" {
