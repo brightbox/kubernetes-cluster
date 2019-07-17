46c93e6e8a88f5e27a4b4c2973265ca986c669ef
Neil Wilson <neil@aldur.co.uk>
2019-07-17 16:23:16 +0100

    Move CA generation into main file

--- main.tf
+++ main.tf
@@ -54,3 +54,28 @@ resource "null_resource" "spread_deployments" {
   }
 
 }
+
+resource "tls_private_key" "k8s_ca" {
+  algorithm = "RSA"
+}
+
+resource "tls_self_signed_cert" "k8s_ca" {
+  key_algorithm   = tls_private_key.k8s_ca.algorithm
+  private_key_pem = tls_private_key.k8s_ca.private_key_pem
+
+  subject {
+    common_name         = "apiserver"
+    organizational_unit = local.cluster_fqdn
+  }
+
+  validity_period_hours = local.validity_period
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "cert_signing",
+  ]
+
+  is_ca_certificate = true
+}
+
