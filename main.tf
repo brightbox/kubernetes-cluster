832f90e5d18f7cc9b75b3f75c91b14cfa6635875
Neil Wilson <neil@aldur.co.uk>
2020-04-15 11:10:50 +0100

    Move version stuff to version file

--- main.tf
+++ main.tf
@@ -12,32 +12,6 @@ locals {
   storage_node_ids = module.k8s_storage.servers[*].id
 }
 
-provider "brightbox" {
-  version   = "~> 1.2"
-  apiurl    = "https://api.${var.region}.brightbox.com"
-  username  = var.username
-  password  = var.password
-  account   = var.account
-  apiclient = var.apiclient
-  apisecret = var.apisecret
-}
-
-provider "null" {
-  version = "~> 2.0"
-}
-
-provider "random" {
-  version = "~> 2.0"
-}
-
-provider "tls" {
-  version = "~> 2.0.1"
-}
-
-provider "template" {
-  version = "~> 2.1"
-}
-
 resource "brightbox_api_client" "controller_client" {
   name              = "Cloud Controller ${local.cluster_fqdn}"
   permissions_group = "full"
