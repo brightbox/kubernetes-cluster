4f664912498a7b766555d0a5ca290ebdccaec97e
Neil Wilson <neil@aldur.co.uk>
2020-04-15 10:36:18 +0100

    Move version stuff to version file

--- main.tf
+++ main.tf
@@ -26,32 +26,6 @@ EOT
 EOT
 }
 
-provider "brightbox" {
-  version   = "~> 1.2"
-  apiurl    = "https://api.${var.region}.brightbox.com"
-  username  = var.username
-  password  = var.password
-  account   = var.account
-  apiclient = var.apiclient
-  apisecret = var.apisecret
-}
-
-provider "null" {
-  version = "~> 2.0"
-}
-
-provider "random" {
-  version = "~> 2.0"
-}
-
-provider "tls" {
-  version = "~> 2.0.1"
-}
-
-provider "template" {
-  version = "~> 2.1"
-}
-
 resource "null_resource" "spread_deployments" {
 
   depends_on = [
