b99e2ce9cf79c2f17b2e49f20c6f3cce01cb6f27
Neil Wilson <neil@aldur.co.uk>
2020-08-19 15:06:34 +0100

    Add Terraform 0.13 versions

--- master/versions.tf
+++ master/versions.tf
@@ -1,16 +1,20 @@
 terraform {
   required_providers {
     brightbox = {
-      source = "brightbox/brightbox"
+      source  = "brightbox/brightbox"
       version = "~> 1.4.3"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "~> 2.1.2"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 2.3.0"
+    }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 1.22.1"
     }
   }
   required_version = ">= 0.12"
