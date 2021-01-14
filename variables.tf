9d0d688c881de9193c6083e18dd3bf9133464cf6
Neil Wilson <neil@aldur.co.uk>
2021-01-14 17:13:45 +0000

    Bump default version to 1.20.2

--- variables.tf
+++ variables.tf
@@ -180,7 +180,7 @@ variable "storage_system" {
 variable "kubernetes_release" {
   type        = string
   description = "Version of Kubernetes to install"
-  default     = "1.20.1"
+  default     = "1.20.2"
 }
 
 variable "autoscaler_release" {
