75eb6154f0e4d4447050d4faa136e3b5f4ff1a14
Neil Wilson <neil@aldur.co.uk>
2021-12-02 08:00:38 +0000

    Update default versions
    - Default Kubernetes version: 1.22.4
    - Default Autoscaler version: 1.22.1
    - Default Calico version: 3.21

--- variables.tf
+++ variables.tf
@@ -180,19 +180,19 @@ variable "storage_system" {
 variable "kubernetes_release" {
   type        = string
   description = "Version of Kubernetes to install"
-  default     = "1.22.2"
+  default     = "1.22.4"
 }
 
 variable "autoscaler_release" {
   type        = string
   description = "Version of Vertical Autoscaler to install if the scaling features are activated. This needs to be the same minor version as the k8s release"
-  default     = "1.22.0"
+  default     = "1.22.1"
 }
 
 variable "calico_release" {
   type        = string
   description = "Version of Calico plugin to install"
-  default     = "3.20"
+  default     = "3.21"
 }
 
 # Boot image search string
