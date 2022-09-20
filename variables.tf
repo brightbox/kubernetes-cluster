8efcdb279d17a8eff2d04aa12af62cc4162048a4
Neil Wilson <neil@aldur.co.uk>
2022-09-20 10:39:19 +0100

    Update default k8s to 1.25

--- variables.tf
+++ variables.tf
@@ -180,13 +180,13 @@ variable "storage_system" {
 variable "kubernetes_release" {
   type        = string
   description = "Version of Kubernetes to install"
-  default     = "1.24.4"
+  default     = "1.25.1"
 }
 
 variable "autoscaler_release" {
   type        = string
   description = "Version of Vertical Autoscaler to install if the scaling features are activated. This needs to be the same minor version as the k8s release"
-  default     = "1.24.0"
+  default     = "1.25.0"
 }
 
 variable "calico_release" {
