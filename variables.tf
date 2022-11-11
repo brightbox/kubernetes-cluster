88932ac9c7b8373e1f6bc7a8d360dc7bad2925bf
Neil Wilson <neil@aldur.co.uk>
2022-11-11 11:12:06 +0000

    Update default k8s to 1.25.4

--- variables.tf
+++ variables.tf
@@ -180,7 +180,7 @@ variable "storage_system" {
 variable "kubernetes_release" {
   type        = string
   description = "Version of Kubernetes to install"
-  default     = "1.25.3"
+  default     = "1.25.4"
 }
 
 variable "autoscaler_release" {
