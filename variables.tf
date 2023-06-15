8dde812a1a650a9da4a2c5231162c61c8e7f08e7
Neil Wilson <neil@aldur.co.uk>
2023-06-15 11:24:44 +0100

    Update versions
    k8s: 1.27.3

--- variables.tf
+++ variables.tf
@@ -186,7 +186,7 @@ variable "storage_system" {
 variable "kubernetes_release" {
   type        = string
   description = "Version of Kubernetes to install"
-  default     = "1.27.2"
+  default     = "1.27.3"
 }
 
 variable "critools_release" {
