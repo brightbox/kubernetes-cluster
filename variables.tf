006065f030aaf6d69b40f5f660994807bf1e5c44
Neil Wilson <neil@aldur.co.uk>
2024-05-17 09:12:37 +0100

    Update to version 1.30.1

--- variables.tf
+++ variables.tf
@@ -186,25 +186,25 @@ variable "storage_system" {
 variable "kubernetes_release" {
   type        = string
   description = "Version of Kubernetes to install"
-  default     = "1.29.4"
+  default     = "1.30.1"
 }
 
 variable "critools_release" {
   type        = string
   description = "Version of cri-tools to install"
-  default     = "1.29"
+  default     = "1.30"
 }
 
 variable "autoscaler_release" {
   type        = string
   description = "Version of Vertical Autoscaler to install if the scaling features are activated. This needs to be the same minor version as the k8s release"
-  default     = "1.29.0"
+  default     = "1.30.0"
 }
 
 variable "calico_release" {
   type        = string
   description = "Version of Calico plugin to install"
-  default     = "3.27.2"
+  default     = "3.28.0"
 }
 
 # Container Registry
